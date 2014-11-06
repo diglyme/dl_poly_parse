298400435b3a998383d698b7700b094f3d811ed2
Keir Little <me@keirlittle.com>
2014-11-06 12:03:17 +0000

    indented with spaces

--- dl_poly_parse.py
+++ dl_poly_parse.py
@@ -15,84 +15,84 @@ PARSED = "PARSED"
 BREAK = " ------------------------------------------------------------------------------------------------------------------------\n"
 
 def getLines():
-	with open(OUTPUT, "r") as f:
-		lines = f.readlines()
-	return lines
+    with open(OUTPUT, "r") as f:
+        lines = f.readlines()
+    return lines
 
 def getHeaders():
-	lines = getLines()
-	firstBreak = lines.index(BREAK)
-	headers = lines[firstBreak+2].split() + lines[firstBreak+3].split() + lines[firstBreak+4].split()
-	headers.remove("(s)")
-	headers[headers.index("cpu")] = "cpu (s)"
-	return headers
+    lines = getLines()
+    firstBreak = lines.index(BREAK)
+    headers = lines[firstBreak+2].split() + lines[firstBreak+3].split() + lines[firstBreak+4].split()
+    headers.remove("(s)")
+    headers[headers.index("cpu")] = "cpu (s)"
+    return headers
 
 
 def getProperty(prop):
-	lines = getLines()
-	headers = getHeaders()
+    lines = getLines()
+    headers = getHeaders()
 
-	# truncate from first BREAK
-	lines = lines[lines.index(BREAK):]
+    # truncate from first BREAK
+    lines = lines[lines.index(BREAK):]
 
-	propIndex = headers.index(prop)
-	propList = []
+    propIndex = headers.index(prop)
+    propList = []
 
-	for i, l in enumerate(lines):
-		if l == BREAK and len(lines[i+1].split()) == 10:
-			values = lines[i+1].split() + lines[i+2].split() + lines[i+3].split()
-			propList.append(values[propIndex])
+    for i, l in enumerate(lines):
+        if l == BREAK and len(lines[i+1].split()) == 10:
+            values = lines[i+1].split() + lines[i+2].split() + lines[i+3].split()
+            propList.append(values[propIndex])
 
-	return propList
+    return propList
 
 def sortList(unsorted):
-	# returns list reading down each column of 3 in OUTPUT rather than across each row
-	# this puts certain values usefully adjacent to each other e.g. time, step, cpu
-	# but separates others e.g. alpha, beta, gamma
-	sort = []
-	for i in range(0,len(unsorted)):
-		triple = unsorted[i::10]
-		for j in triple:
-			sort.append(j)
-	return sort[:30]
+    # returns list reading down each column of 3 in OUTPUT rather than across each row
+    # this puts certain values usefully adjacent to each other e.g. time, step, cpu
+    # but separates others e.g. alpha, beta, gamma
+    sort = []
+    for i in range(0,len(unsorted)):
+        triple = unsorted[i::10]
+        for j in triple:
+            sort.append(j)
+    return sort[:30]
 
 def getAllProps():
-	# returns physical properties as a huge list of lists
+    # returns physical properties as a huge list of lists
 
-	lines = getLines()
-	properties = []
+    lines = getLines()
+    properties = []
 
-	for i, l in enumerate(lines):
-		if l == BREAK and len(lines[i+1].split()) == 10: # data always found in lines of 10 after BREAK
-			values = lines[i+1].split() + lines[i+2].split() + lines[i+3].split()
-			
-			if properties == []:	# fill with lists of initial values if empty
-				properties = [[float(v)] for v in values]
-			else: 					# append otherwise
-				for j, p in enumerate(properties):
-					p.append(float(values[j]))
+    for i, l in enumerate(lines):
+        if l == BREAK and len(lines[i+1].split()) == 10: # data always found in lines of 10 after BREAK
+            values = lines[i+1].split() + lines[i+2].split() + lines[i+3].split()
+            
+            if properties == []:    # fill with lists of initial values if empty
+                properties = [[float(v)] for v in values]
+            else:                   # append otherwise
+                for j, p in enumerate(properties):
+                    p.append(float(values[j]))
 
-	return properties
-		# could optimise by initialising each list with zeroes
+    return properties
+        # could optimise by initialising each list with zeroes
 
 def parse():
-	headers = getHeaders()
-	properties = getAllProps()
-	n = len(properties[0])
-	sortedHeaders = sortList(headers)
-	sortedProps = sortList(properties)
+    headers = getHeaders()
+    properties = getAllProps()
+    n = len(properties[0])
+    sortedHeaders = sortList(headers)
+    sortedProps = sortList(properties)
 
-	parsed = ""
-	for h in sortedHeaders:
-		parsed += "%-12s" % (h)
-	for i in range(0,n):
-		parsed += "\n"
-		for p in sortedProps:
-			parsed += "%-11s " % (p[i])
+    parsed = ""
+    for h in sortedHeaders:
+        parsed += "%-12s" % (h)
+    for i in range(0,n):
+        parsed += "\n"
+        for p in sortedProps:
+            parsed += "%-11s " % (p[i])
 
 
-	with open(PARSED, "w") as f:
-		f.write(parsed)
+    with open(PARSED, "w") as f:
+        f.write(parsed)
 
 if __name__ == '__main__':
-	parse()
+    parse()
