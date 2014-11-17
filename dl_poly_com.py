f657e0ed94d8cca121158aefeff9f7f94e9bd4c1
Keir Little <me@keirlittle.com>
2014-11-17 14:11:29 +0000

    calculates cage CoMs on each step

--- dl_poly_com.py
+++ dl_poly_com.py
@@ -8,6 +8,7 @@
 # To do:
 #  * Calculate cage CoMs at each step, compare time difference
 #  * Use file.readline(), file.tell() and file.seek() instead of reading all lines to conserve memory
+#  * Allow compatibility with re-started HISTORY files
 
 from numpy import array, where, square, sqrt
 
@@ -72,11 +73,12 @@ def main():
 	# each atom takes 2 lines, plus 4 header lines per timestep
 	step_lines = (cage_atoms + guest_atoms) * 2 + 4
 
-	cages_x, cages_y, cages_z = cage_centres(lines[6:6+cage_atoms*2])
+	# cages_x, cages_y, cages_z = cage_centres(lines[6:6+cage_atoms*2])
 
 	for i, line in enumerate(lines):
 		if (i-2) % step_lines == 0: # first timstep comes after two header lines
 			step = int(line.split()[1])
+            cages_x, cages_y, cages_z = cage_centres(lines[i+4:i+4+cage_atoms*2])
 			guest_x, guest_y, guest_z = centre_of_mass(lines[i+4+cage_atoms*2:i+step_lines]) # gets guest CoM
 			current_cage = in_cage(guest_x, guest_y, guest_z, cages_x, cages_y, cages_z)
 			output.append(str(step)+" "+str(round(guest_x, 4))+" "+str(round(guest_y, 4))+" "+str(round(guest_z, 4))+" "+str(current_cage)+"\n")
