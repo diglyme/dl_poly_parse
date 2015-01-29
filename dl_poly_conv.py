44a473567c3afbc0ed81979af7d046a3aed3b476
diglyme <diglyme@users.noreply.github.com>
2015-01-29 16:07:56 +0000

    s/camel/_/, optimised parse

--- dl_poly_conv.py
+++ dl_poly_conv.py
@@ -9,43 +9,38 @@ REVCON = "REVCON"
 DIR = os.getcwd().split("/")[-1] # by default, use directory name as file name
 # TYPES
 
-def getLines():
-    with open(REVCON, "r") as f:
-        lines = f.readlines()
-    return lines
-
-def getAtoms(lines):
+def get_atoms():
     # returns atoms as a list of dicts with id, atom type, x, y z
     atoms = []
-    for i, l in enumerate(lines):
-        if len(l.split()) == 2 and len(lines[i+1].split()) == 3:
-            l1 = l.split()
-            l2 = lines[i+1].split()
-            # Adds a dict for each atom to list of atoms
-            atoms.append(dict(zip(["index", "atom", "x", "y", "z"],
-                                  [l1[1], l1[0], l2[0], l2[1], l2[2]])))
+    with open(REVCON, "r") as _f:
+        lines = _f.readlines()
+        for i, line in enumerate(lines):
+            if len(line) == 43:
+                l1 = line.split()
+                l2 = lines[i+1].split()
+                # Adds a dict for each atom to list of atoms
+                atoms.append(dict(zip(["index", "atom", "x", "y", "z"],
+                                      [l1[1], l1[0], l2[0], l2[1], l2[2]])))
 
     return atoms
 
 def xyz():
-    atoms = getAtoms(getLines())
+    atoms = get_atoms()
 
     # xyz file begins with number of atoms and a comment line
-    xyzoutput = "%i\n%s\n" % (len(atoms), DIR)
+    xyz_output = "%i\n%s\n" % (len(atoms), DIR)
 
     for atom in atoms:
         atom["atom"] = atom["atom"][0].capitalize()
-        xyzoutput += " ".join([atom["atom"], atom["x"], atom["y"], atom["z"]]) + "\n"
+        xyz_output += " ".join([atom["atom"], atom["x"], atom["y"], atom["z"]]) + "\n"
 
-    with open(DIR+".xyz", "w") as f:
-        f.write(xyzoutput)
-
-
+    with open(DIR+".xyz", "w") as _f:
+        _f.write(xyz_output)
 
 def pdb():
-    atoms = getAtoms(getLines())
+    atoms = get_atoms()
 
-    pdboutput = ""
+    pdb_output = ""
 
     for atom in atoms:
         atom["atom"] = atom["atom"][0].capitalize()
@@ -53,10 +48,10 @@ def pdb():
         # atom["x"] = str(round(float(atom["x"]), 5))[8:]
         # atom["y"] = str(round(float(atom["y"]), 5))[8:]
         # atom["z"] = str(round(float(atom["z"]), 5))[8:]
-        pdboutput += "HETATM%5s %-4s UNK          %8s%8s%8s 1.000 0.000          %-2s\n" % (atom["index"], atom["atom"], atom["x"][:8], atom["y"][:8], atom["z"][:8], atom["atom"])
+        pdb_output += "HETATM%5s %-4s UNK          %8s%8s%8s 1.000 0.000          %-2s\n" % (atom["index"], atom["atom"], atom["x"][:8], atom["y"][:8], atom["z"][:8], atom["atom"])
 
-    with open(DIR+".pdb", "w") as f:
-        f.write(pdboutput)
+    with open(DIR+".pdb", "w") as _f:
+        _f.write(pdb_output)
 
 def main():
     args = sys.argv
@@ -70,6 +65,7 @@ def main():
         return 0
 
     print("Please use 'xyz' or 'pdb' options to choose output format.")
+    return 1
 
 if __name__ == "__main__":
     main()
